e5b4dafdebc9a80a9722d7c68bb47817dc2fddb5
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-05-20 11:32:11 +0100

    Write build_tree method which returns the root node with references to left and right subtrees

--- bst.rb
+++ bst.rb
@@ -21,14 +21,24 @@ class Tree
 
   include MergeSort
 
+  attr_reader :root
+  
   def initialize(values_array = [])
-    @values_array = prepare_array(values_array)
+    array_to_use = prepare_array(values_array)
     # values_array sorted with no duplicates
-    @root = build_tree
+    @root = build_tree(array_to_use)
   end
 
-  def build_tree
-
+  def build_tree(array)
+    return nil if array.size.zero?
+    mid_index = array.size / 2
+    mid_value = array[mid_index]
+    left_values = array.slice!(0, mid_index)
+    array.shift
+    # shift removes the middle value of the original array here
+    right_values = array
+    # we modified array in place so right_values is what remains
+    root = Node.new(mid_value, build_tree(left_values), build_tree(right_values))
   end
 
   def prepare_array(any_array)
@@ -38,3 +48,5 @@ class Tree
 
 end
 
+my_tree = Tree.new([1,3,2])
+p my_tree.root
